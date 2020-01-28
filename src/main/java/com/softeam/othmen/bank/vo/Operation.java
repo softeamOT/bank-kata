ebfa5f216641531f82b8e756ff5e53fdb7fcde9e
user <othmen.tiliouine@softeam.fr>
2020-01-28 15:56:54 +0100

    Operation equals impl

--- src/main/java/com/softeam/othmen/bank/vo/Operation.java
+++ src/main/java/com/softeam/othmen/bank/vo/Operation.java
@@ -35,4 +35,31 @@ public class Operation {
 		return dateTime;
 	}
 	
+	@Override
+	public int hashCode() {
+		int result = type != null ? type.hashCode() : 0;
+		result = 31 * result + (amount != null ? amount.hashCode() : 0);
+		result = 17 * result + (dateTime != null ? dateTime.hashCode() : 0);
+		return result;
+	}	
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Operation operation = (Operation) o;
+
+		if (type != null ? !type.equals(operation.type) : operation.type != null) {
+			return false;
+		}
+		if (amount != null ? !amount.equals(operation.amount) : operation.amount != null) {
+			return false;
+		}
+		if (dateTime != null ? !dateTime.equals(operation.dateTime) : operation.dateTime != null) {
+			return false;
+		}
+		return true;
+	}
+
 }
